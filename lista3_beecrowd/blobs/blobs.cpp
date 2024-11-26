47990efd2ddae91378c11df8fbdc945cdfbd312b
Matheus cunha <eusouotito@gmail.com>
2024-11-26 18:57:49 -0300

    criar o arquivo do ex3.

--- lista3_beecrowd/blobs/blobs.cpp
+++ lista3_beecrowd/blobs/blobs.cpp
@@ -6,16 +6,18 @@ using namespace std;
 int main() {
     int m, n;
     float kg;
+    cout << "numero de casos de teste:\n";
     cin >> n;
     if ((1 <= n && n <= 1000)){
         for(m = 0; m < n; m++){
+            cout << "comida blobs:\n";
             cin >> kg;
             if(kg > 1){
                 while(kg > 1){
-                (kg / 2)
-            }
-            }else{
-                cout << "1 dia"
+                (kg / 2);
+                }
+            }else if (n <= 1 && n > 0){
+                cout << "1 dia\n";
             }
             cout << setprecision(2) << kg << " dias" << endl;
         }
