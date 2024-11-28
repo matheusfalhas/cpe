e2c66b14ae30c3d5e0351bbdda4daa4dd2e39ec4
Matheus Cunha <eusouotito@gmail.com>
2024-11-27 22:24:58 -0300

    terminar (?) o codigo par_impar.

--- lista3_beecrowd/par_impar/par_impar.cpp
+++ lista3_beecrowd/par_impar/par_impar.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 using namespace std;
 
 int main (){
-    
+    int n, a, b, i, count = 1;
+    string nome1, nome2;
+    while(cin >> n){
+        if(n == 0)  break;
+        cin >> nome1 >> nome2;
+        cout << "Teste " << count << "\n";
+        for(i = 0; i < n; i++){
+            cin >> a >> b;
+            if(((a+b)%2) == 0){
+                cout << nome1 << "\n"; 
+            }else{
+                cout << nome2 << "\n";
+            }
+        }
+        count++;
+    }
 }
